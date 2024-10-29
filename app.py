9b7149e1cbf91bf4e78f5c70f33ed45d2e7a9cd8
Manas Dhir <manas.dhir2004@gmail.com>
2024-10-29 23:49:52 +0530

    added dropdown

--- app.py
+++ app.py
@@ -30,105 +30,112 @@ sample_df = pd.DataFrame(sample_data)
 def main():
     st.title("Capsule Vision Challenge 2024 Sanity Checker")
 
-    st.write("### Guidelines for Uploading Excel file")
-    st.markdown("""
-    1. **image_path** column should contain only the image name (e.g., `image.jpg`).
-    2. **Predicted probabilities** for each class should be present along with the `predicted_class` column.
-    3. **Predictions for all images** must be complete, with no blanks or missing values.
-    4. Correct file can be generated from the code provided [here](https://github.com/misahub2023/Capsule-Vision-2024-Challenge/blob/main/sample_codes_for_participants/Evaluate_model.py)
-    5. Excel file name should be same as team name.
-    """)
+    # Dropdown to select mode (Test, Training, or Validation)
+    mode = st.selectbox("Select Mode", ["Test", "Training", "Validation"])
 
-    st.write("### Example of the Expected Excel Format")
-    st.dataframe(sample_df, hide_index=True)
+    if mode == "Test":
+        st.write("### Guidelines for Uploading Excel file")
+        st.markdown("""
+        1. **image_path** column should contain only the image name (e.g., `image.jpg`).
+        2. **Predicted probabilities** for each class should be present along with the `predicted_class` column.
+        3. **Predictions for all images** must be complete, with no blanks or missing values.
+        4. Correct file can be generated from the code provided [here](https://github.com/misahub2023/Capsule-Vision-2024-Challenge/blob/main/sample_codes_for_participants/Evaluate_model.py)
+        5. Excel file name should be same as team name.
+        """)
 
-    uploaded_file = st.file_uploader("Upload your CSV file", type=["xlsx"])
-    
-    if uploaded_file is not None:
-        passed_all_checks = True  
+        st.write("### Example of the Expected Excel Format")
+        st.dataframe(sample_df, hide_index=True)
 
-        with st.spinner("Checking..."):
-            try:
-                df = pd.read_excel(uploaded_file)
-            except Exception as e:
-                st.error(f"Error reading the file: {e}")
-                return
+        uploaded_file = st.file_uploader("Upload your CSV file", type=["xlsx"])
 
-            if 'image_path' not in df.columns:
-                st.error("The 'image_path' column is missing from the uploaded file.")
-                passed_all_checks = False
-                return
-            try:
-                reference_df = pd.read_csv(REFERENCE_FILE_PATH)
-                reference_images = set(reference_df["file_name"])
-            except Exception as e:
-                st.error(f"Error reading the reference file: {e}")
-                return
+        if uploaded_file is not None:
+            passed_all_checks = True  
 
-            predicted_images = set(df["image_path"])
-            missing_images = reference_images - predicted_images
-            extra_images = predicted_images - reference_images
+            with st.spinner("Checking..."):
+                try:
+                    df = pd.read_excel(uploaded_file)
+                except Exception as e:
+                    st.error(f"Error reading the file: {e}")
+                    return
 
-            if len(missing_images) > 10:
-                st.error("Image names do not match; too many missing images.")
-                passed_all_checks = False
-            elif missing_images:
-                st.error(f"The following images are missing from the predictions: {', '.join(missing_images)}")
-                passed_all_checks = False
+                if 'image_path' not in df.columns:
+                    st.error("The 'image_path' column is missing from the uploaded file.")
+                    passed_all_checks = False
+                    return
+                try:
+                    reference_df = pd.read_csv(REFERENCE_FILE_PATH)
+                    reference_images = set(reference_df["file_name"])
+                except Exception as e:
+                    st.error(f"Error reading the reference file: {e}")
+                    return
 
-            if len(extra_images) > 10:
-                st.error("There are extra images in the predictions.")
-                passed_all_checks = False
-            elif extra_images:
-                st.error(f"The following images are extra and not in the reference file: {', '.join(extra_images)}")
-                passed_all_checks = False
+                predicted_images = set(df["image_path"])
+                missing_images = reference_images - predicted_images
+                extra_images = predicted_images - reference_images
 
-            missing_columns = [col for col in EXPECTED_COLUMNS if col not in df.columns]
-            extra_columns = [col for col in df.columns if col not in EXPECTED_COLUMNS]
-            
-            if missing_columns:
-                st.error(f"The following columns are missing: {', '.join(missing_columns)}")
-                passed_all_checks = False
+                if len(missing_images) > 10:
+                    st.error("Image names do not match; too many missing images.")
+                    passed_all_checks = False
+                elif missing_images:
+                    st.error(f"The following images are missing from the predictions: {', '.join(missing_images)}")
+                    passed_all_checks = False
 
-            if extra_columns:
-                st.error(f"The following columns are not expected: {', '.join(extra_columns)}")
-                passed_all_checks = False
+                if len(extra_images) > 10:
+                    st.error("There are extra images in the predictions.")
+                    passed_all_checks = False
+                elif extra_images:
+                    st.error(f"The following images are extra and not in the reference file: {', '.join(extra_images)}")
+                    passed_all_checks = False
 
-            if df.isnull().values.any():
-                st.error("The CSV contains missing values.")
-                missing_rows = df[df.isnull().any(axis=1)]["image_path"].tolist()
-                if len(missing_rows) > 10:
-                    st.write("Too many rows with missing predictions.")
-                else:
-                    st.write(f"Missing values found in rows with image_path: {', '.join(missing_rows)}")
-                passed_all_checks = False
+                missing_columns = [col for col in EXPECTED_COLUMNS if col not in df.columns]
+                extra_columns = [col for col in df.columns if col not in EXPECTED_COLUMNS]
+                
+                if missing_columns:
+                    st.error(f"The following columns are missing: {', '.join(missing_columns)}")
+                    passed_all_checks = False
 
-            invalid_classes = df[~df['predicted_class'].isin(VALID_CLASSES)]
-            if not invalid_classes.empty:
-                st.error("Some `predicted_class` values are invalid.")
-                invalid_rows = invalid_classes["image_path"].tolist()
-                if len(invalid_rows) > 10:
-                    st.write("Too many invalid predicted class values.")
-                else:
-                    st.write(f"Invalid `predicted_class` entries found for image_path: {', '.join(invalid_rows)}")
-                passed_all_checks = False
+                if extra_columns:
+                    st.error(f"The following columns are not expected: {', '.join(extra_columns)}")
+                    passed_all_checks = False
 
-            if df['image_path'].duplicated().any():
-                st.error("Duplicate image paths found.")
-                duplicated_rows = df[df['image_path'].duplicated()]["image_path"].tolist()
-                if len(duplicated_rows) > 10:
-                    st.write("Too many duplicate image paths found.")
-                else:
-                    st.write(f"Duplicate entries found for image_path: {', '.join(duplicated_rows)}")
-                passed_all_checks = False
+                if df.isnull().values.any():
+                    st.error("The CSV contains missing values.")
+                    missing_rows = df[df.isnull().any(axis=1)]["image_path"].tolist()
+                    if len(missing_rows) > 10:
+                        st.write("Too many rows with missing predictions.")
+                    else:
+                        st.write(f"Missing values found in rows with image_path: {', '.join(missing_rows)}")
+                    passed_all_checks = False
 
-        if passed_all_checks:
-            st.success("All checks passed.")
-        else:
-            st.error("File did not pass all checks. Please see the errors above.")
+                invalid_classes = df[~df['predicted_class'].isin(VALID_CLASSES)]
+                if not invalid_classes.empty:
+                    st.error("Some `predicted_class` values are invalid.")
+                    invalid_rows = invalid_classes["image_path"].tolist()
+                    if len(invalid_rows) > 10:
+                        st.write("Too many invalid predicted class values.")
+                    else:
+                        st.write(f"Invalid `predicted_class` entries found for image_path: {', '.join(invalid_rows)}")
+                    passed_all_checks = False
 
-        del df, reference_df, missing_columns, extra_columns, missing_images, extra_images, invalid_classes, uploaded_file
-        gc.collect()
+                if df['image_path'].duplicated().any():
+                    st.error("Duplicate image paths found.")
+                    duplicated_rows = df[df['image_path'].duplicated()]["image_path"].tolist()
+                    if len(duplicated_rows) > 10:
+                        st.write("Too many duplicate image paths found.")
+                    else:
+                        st.write(f"Duplicate entries found for image_path: {', '.join(duplicated_rows)}")
+                    passed_all_checks = False
+
+            if passed_all_checks:
+                st.success("All checks passed.")
+            else:
+                st.error("File did not pass all checks. Please see the errors above.")
+
+            del df, reference_df, missing_columns, extra_columns, missing_images, extra_images, invalid_classes, uploaded_file
+            gc.collect()
+
+    elif mode == "Training" or mode == "Validation":
+        st.write(f"You selected **{mode}** mode. Logic for this mode will be implemented soon.")
 
 if __name__ == "__main__":
     main()
